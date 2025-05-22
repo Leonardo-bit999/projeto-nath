17a9377ea6bd998c9bc6388037521333e9d1ad13
Leonado-bit999 <leosantos.silva9925@gmail.com>
2025-05-21 21:16:56 -0300

    Vite configuração

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/projeto-nath/', // 👈 ISSO É ESSENCIAL
+  base: './', // pode ser './' mesmo na Vercel
   plugins: [react()],
-});
+})
